eb8f49114bd6241bb746641e58fd3ce98d3933c3
hackerudroids <tomerjava@gmail.com>
2017-03-13 21:15:03 +0200

    Ver 01.01

--- src/com/arichafamily/java/Main.java
+++ src/com/arichafamily/java/Main.java
@@ -1,10 +1,13 @@
 package com.arichafamily.java;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter employee name: ");
     }
 }
